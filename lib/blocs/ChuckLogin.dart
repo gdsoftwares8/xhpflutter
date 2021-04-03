806fdc99b50319bd429b17dd34d3398ff3c9c0b3
sumeet field <sumeetfield@gmail.com>
2021-04-03 21:02:26 +0530

    changes

--- lib/blocs/ChuckLogin.dart
+++ lib/blocs/ChuckLogin.dart
@@ -1,44 +1,59 @@
 import 'dart:async';
 import 'dart:collection';
 
+import 'package:get/get.dart';
+import 'package:xhp/blocs/ChuckLocalData.dart';
 import 'package:xhp/models/login_response.dart';
 
-import 'package:xhp/networking/Response.dart';
+import 'package:xhp/networking/Response.dart' as res;
 import 'package:xhp/repository/ChuckLoginRepo.dart';
 
 import 'package:xhp/utils/GlobalFuncs.dart';
 
+import '../controllers/user_controller.dart';
+import '../models/user.dart';
 
 class ChuckLoginbloc {
+  User user;
   ChuckLoginRepository _chuckRepository;
-  StreamController _chuckListController = StreamController<Response<LoginResponce>>();
+  StreamController _chuckListController =
+      StreamController<res.Response<LoginResponce>>();
 
-  Stream<Response<LoginResponce>> get chuckListStream =>
+  Stream<res.Response<LoginResponce>> get chuckListStream =>
       _chuckListController.stream;
 
   ChuckLoginbloc() {
-   // _chuckListController = StreamController.broadcast();
+    // _chuckListController = StreamController.broadcast();
     _chuckRepository = ChuckLoginRepository();
   }
 
   fetchLogin(String username, String password) async {
-  //  _chuckListController.add("test");
-   _chuckListController.add(Response<LoginResponce>.loading('Getting Login.'));
+    //  _chuckListController.add("test");
+    _chuckListController
+        .add(res.Response<LoginResponce>.loading('Getting Login.'));
     try {
-      Map<String,String> map = HashMap();
+      Map<String, String> map = HashMap();
       map.putIfAbsent("username", () => username);
       map.putIfAbsent("password", () => password);
-     // map.putIfAbsent("member_id", () => memberId);
-      LoginResponce chuckDatas =
-      await _chuckRepository.fetchLogin(postParams: map);
-    //  GlobalFunc.logPrint("Login Responce $chuckDatas");
-      if(chuckDatas.status == 1)
-        _chuckListController.add(Response<LoginResponce>.completed(chuckDatas));
-      else
-        _chuckListController.add(Response<LoginResponce>.error(chuckDatas.message));
+      // map.putIfAbsent("member_id", () => memberId);
+
+      await _chuckRepository
+          .fetchLogin(postParams: map)
+          .then((LoginResponce chuckDatas) {
+        if (chuckDatas.status == 1) {
+          ChuckLocalData.saveUser(chuckDatas.userData);
+          _chuckListController
+              .add(res.Response<LoginResponce>.completed(chuckDatas));
+        } else {
+          _chuckListController
+              .add(res.Response<LoginResponce>.error(chuckDatas.message));
+        }
+      });
+      //  GlobalFunc.logPrint("Login Responce $chuckDatas");
+
     } catch (e) {
       GlobalFunc.logPrint("Error $e");
-      _chuckListController.add(Response<LoginResponce>.error(e.toString()));
+      _chuckListController.add(res.Response<LoginResponce>.error(e.toString()));
       print(e);
     }
   }
---
+++ lib/controllers/user_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+import '../models/user.dart';
+
+class UserInfoController extends GetxController {
+  var user = User().obs;
+}
